6480af716f93802de4da210420d10dc79920c39a
chikoti srujan <137102659+chikotisrujan@users.noreply.github.com>
2025-01-08 10:23:02 +0530

    Add files via upload

---
+++ DAY_1.py
@@ -0,0 +1,8 @@
+integer_var = 10             
+float_var = 10.5             
+string_var = "Hello World"   
+boolean_var = True           
+print("Integer Variable:", integer_var, "Type:", type(integer_var))
+print("Float Variable:", float_var, "Type:", type(float_var))
+print("String Variable:", string_var, "Type:", type(string_var))
+print("Boolean Variable:", boolean_var, "Type:", type(boolean_var))
---
+++ DAY_2.py
@@ -0,0 +1,9 @@
+my_list = [10, "apple", 3.14, True, "banana"]
+print("First element (index 0):", my_list[0]) 
+print("Second element (index 1):", my_list[1])  
+print("Last element (index -1):", my_list[-1])  
+
+my_tuple = (10, "apple", 3.14, True, "banana")
+print("First element (index 0):", my_tuple[0]) 
+print("Third element (index 2):", my_tuple[2]) 
+print("Last element (index -1):", my_tuple[-1]) 
