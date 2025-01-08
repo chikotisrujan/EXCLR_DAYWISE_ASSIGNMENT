a60fd5ba5eb2134825041d352ac7402f9c4b6495
chikoti srujan <137102659+chikotisrujan@users.noreply.github.com>
2025-01-08 10:26:11 +0530

    Add files via upload

---
+++ DAY_4.py.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n = int(input("Enter a positive integer n: "))
+even_sum = 0
+for i in range(2, n + 1, 2): 
+    even_sum += i  
+print(f"The sum of all even numbers between 1 and {n} is: {even_sum}")
+
+
+# In[ ]:
+
+
+
+
